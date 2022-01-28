c51dc9486a23d1bedac4773fa9a6afbfc12f46e8
SimonBucko <bucino.36@gmail.com>
2022-01-28 07:29:35 +0100

    added default strucutre

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,19 @@ import "../styles/globals.css";
 import { ThirwebWeb3Provider } from "@3rdweb/hooks";
 
 const supportedChainIds = [4];
-const connectiors = {
+const connectors = {
   injected: {},
 };
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThirwebWeb3Provider
+      supportedChainIds={supportedChainIds}
+      connectors={connectors}
+    >
+      <Component {...pageProps} />
+    </ThirwebWeb3Provider>
+  );
 }
 
 export default MyApp;
