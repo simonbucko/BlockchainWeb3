c8182b8060751aceef5518073c14adf261dc2bbe
SimonBucko <bucino.36@gmail.com>
2022-01-28 07:20:25 +0100

    added dependencies and secret keys

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ThirwebWeb3Provider } from "@3rdweb/hooks";
+
+const supportedChainIds = [4];
+const connectiors = {
+  injected: {},
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
